5d24c4bf0ab4ff14a62615f28bc064be52f9ae5e
a0099654 <a0099654@u.nus.edu>
2016-12-06 22:34:58 +0800

    [UPDATE]Inserting word into google input box

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 driver.manage().window().maximize();
 driver.get('http://www.google.com');
-// driver.findElement({name:'q'}).sendKeys("stella");
+driver.findElement({name:'q'}).sendKeys("stella");
+driver.findElement({name:'q'}).sendKeys(webdriver.Key.ENTER);
 
 driver.takeScreenshot().then(function(data){
     fs.writeFileSync('img.png', data, 'base64');
